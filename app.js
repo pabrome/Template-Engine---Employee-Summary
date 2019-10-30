9e87abd81b2ab27a0bf10fa8ec2199f344342e37
Patrick Abromeit <patrick.abromeit@gmail.com>
2019-10-30 13:42:55 -0400

    completed inquirer prompts

--- app.js
+++ app.js
@@ -1,36 +1,102 @@
 const inquirer = require('inquirer');
 const jest = require('jest');
+var loopCount = 0
+var team = []
+var x = 0
 
-inquirer.prompt({
-    type: "number",
-    name: "teamsize",
-    message: "How many people are in your team?"
-}).then(function(data){
-    for (var x = 1; x <= data.teamsize; x++){
-
+var basicQuestions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'Employee name:',
+    }, {
+        type: 'input',
+        name: 'id',
+        message: 'Employee ID:',
+    }, {
+        type: 'input',
+        name: 'email',
+        message: 'Employee Email:',
+    }, {
+        type: 'list',
+        name: 'title',
+        message: 'Employee Title:',
+        choices: [
+            "Manager",
+            "Engineer",
+            "Intern"
+        ]
     }
-})
+]
 
-inquirer.prompt([
+var againQuestion = 
+{
+    type: 'confirm',
+    name: 'askagain',
+    message: 'Add another employee?',
+}
+
+var managerQuestion =
     {
-        type: "input",
-        name: "name",
-        message: "Employee name:"
-    },
-    {
-        type: "input",
-        name: "id",
-        message: "Employee ID"
-    },
-    {
-        type: "input",
-        name: "title",
-        message: "Employee Title"
+        type: 'input',
+        name: 'officeNumber',
+        message: 'Office Number:',
     }
-]).then(function(data){
-    console.log(data)
-})
 
+var engineerQuestion = [
+    {
+        type: 'input',
+        name: 'github',
+        message: 'GitHub Username:',
+    }
+]
+
+var studentQuestion = [
+    {
+        type: 'input',
+        name: 'school',
+        message: 'School:',
+    }
+]
+
+ask()
+
+function ask(){
+    inquirer.prompt(basicQuestions).then(function(answers) {
+        team.push(answers) 
+        switch(team[x].title) {
+            case "Manager":
+                inquirer.prompt(managerQuestion).then(function(answers){
+                    team[x].officeNumber = answers.officeNumber
+                    again()
+                })
+                break;
+            case "Engineer":
+                inquirer.prompt(engineerQuestion).then(function(answers){
+                    team[x].github = answers.github
+                    again()
+                })
+                break;
+            case "Intern":
+                inquirer.prompt(studentQuestion).then(function(answers){
+                    team[x].school = answers.school
+                    again()
+                })
+                break;
+            default:
+        }
+    });
+}
+
+function again(){
+    console.log(team)
+    inquirer.prompt(againQuestion).then(function(answers) {
+        if (answers.askagain == true) {
+            x++
+            ask()
+        }
+    })
+}
 
 class Employee{
     constructor(data) {
